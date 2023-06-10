17e2ea3eddad7b46a5961b1ed01ca62a1fd9ff67
Sonsawan <sonsawan.n1994@gmail.com>
2023-06-10 20:01:21 +0700

    set up environment

--- starter-code/JavaScript/navbar.js
+++
@@ -1,9 +0,0 @@
-// Contorl Menu Taggle
-document.addEventListener("DOMContentLoaded", function() {
-  const toggleButton = document.querySelector('[data-collapse-toggle="navbar-default"]');
-  const navbar = document.querySelector('#navbar-default');
-
-  toggleButton.addEventListener("click", function() {
-    navbar.classList.toggle("hidden");
-  });
-});
